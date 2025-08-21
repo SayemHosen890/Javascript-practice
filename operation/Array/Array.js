a8ae6c12d2a517b1b0e4b933c5ce9f64bdb3c850
SayemHosen890 <sayemhosen902@gmail.com>
2025-08-21 14:48:28 +0600

    function

--- operation/Array/Array.js
+++ operation/Array/Array.js
@@ -1,7 +1,79 @@
-let marks = [23, 34, 45, 56, 67, 78];
-console.log(marks);
+// // let marks = [23, 34, 45, 56, 67, 78];
+// // console.log(marks);
 
-let heroes=["spiderman","batman",'goodman',"hulk"];
-console.log(heroes);
+// let heroes = ["spiderman", "batman", "goodman", "hulk"];
+// // console.log(heroes.push("Tangail"));
+
+// for (let hero of heroes) {
+//   // console.log(hero)
+// }
+
+// let cities = ["Dhaka", "Barishal", "chittagong", "sylhet", "Cox Bazar"];
+
+// for (let city of cities) {
+//   // console.log(city)
+// }
+
+// let marks = [45, 56, 78, 90, 55, 81, 95];
+// let sum = 0;
+// for (let mark of marks) {
+//   sum = sum + mark;
+// }
+// let avgmark = sum / marks.length;
+// // console.log(avgmark)
+
+// // let items = [250, 350, 400, 700, 500, 600];
+
+// // let i = 0;
+
+// // for (let val of items) {
+// //   //   console.log(`value at ${i} = ${val}`);
+
+// //   let offer = val / 10;
+// //   items[i] = items[i] - offer;
+// //   // console.log(`after the offer = ${items[i]}`);
+// //   i++;
+// // }
+// let items = [250, 350, 400, 700, 500, 600];
+
+// for (let i = 0; i < items.length; i++) {
+//   let offer = items[i] / 10;
+//   items[i] -= offer;
+// }
+// // console.log(items);
+
+// let fishes = ["katla", "ilish", "puti", "rui", "shark", 1, 2, 4];
+// // console.log(fishes.pop())
+// // console.log(fishes.push("piranha"));
+// // console.log(fishes.toString())
+
+// // let num =[1,2,3,4]
+// let letter = ["a", "b", "c", "d"];
+
+// // console.log(num.concat(letter))
+
+// let num = [1, 2, 3, 4, 5];
+
+// // console.log(num.unshift(-3,-2,-1,0))
+
+// // console.log(num.shift())
+
+// let animals = ["cow", "camel", "goat", "lion", "deer", "dumba"];
+// // console.log(animals.slice(2,4))
+// // console.log(animals.slice(1,4))
+
+// let months = ["jan", "feb", "march", "april", "may"];
+
+// // console.log(months.splice(2, 2, "june"));
+
+// let company = ["Bloomberg", "Microsoft", "Google", "Tesla", "Spacex"];
+
+// // console.log(company.shift());
+// // console.log(company.splice(2,1,"Bkash"))
+// // console.log(company.push("Amazon"))
+
+// // console.log(company.length.toString())
+
+// console.log()
 
 
