7b50ef348028fa754f84b374ee2746ab4847846d
SayemHosen890 <sayemhosen902@gmail.com>
2025-08-21 17:43:52 +0600

    second

---
+++ operation/Function/Function.js
@@ -0,0 +1,18 @@
+function myFunction() {
+  console.log("here is everything ready");
+  console.log("here is everything ready");
+  console.log("here is everything ready");
+}
+// myFunction();
+
+function sum(a, b) {
+  console.log(a * b);
+}
+// sum(5, 5);
+
+function sum(a, b) {
+  add = a + b;
+  return add;
+}
+let added = sum(8, 8);
+console.log(added);
